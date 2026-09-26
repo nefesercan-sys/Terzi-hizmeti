ed7e3703d84e88f271fa2de3c58f115812b1a530
nefesercan-sys <nefesercan@gmail.com>
2026-09-26 09:46:15 +0300

    Add German page for Anavera Tekstil

---
+++ app/de/anavera-tekstil/page.tsx
@@ -0,0 +1,15 @@
+// app/de/anavera-tekstil/page.tsx
+import AnaveraTekstilSayfasi from '@/components/AnaveraTekstilSayfasi';
+import { buildAnaveraMetadata, buildAnaveraJsonLd } from '@/lib/anavera-meta';
+
+export const metadata = buildAnaveraMetadata('de');
+const jsonLd = buildAnaveraJsonLd('de');
+
+export default function Page() {
+  return (
+    <>
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+      <AnaveraTekstilSayfasi lang="de" />
+    </>
+  );
+}
